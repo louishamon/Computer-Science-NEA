5e215478c712d0c4809eb4bda1ac4e62aa3bb4c2
louishamon <louisobhamon@gmail.com>
2024-09-30 10:40:05 +0100

    Update Character.py

--- Character.py
+++ Character.py
@@ -1,5 +1,10 @@
-from Game import Game
-from settings import *
-
-new_game = Game(screen_width, screen_height)
-new_game.play()
+class Character(pygame.sprite.Sprite):
+  def __init__(self, size, pos):
+    super().__init__()
+    self.movement_speed = 4
+    self.current_weapon = None
+    self.hp = 100
+    self.image = self.image = pygame.Surface((size[0], size[1]))
+    self.rect = self.image.get_rect(topleft = pos)
+    self.x_direction = 0
+    self.y_direction = 0
